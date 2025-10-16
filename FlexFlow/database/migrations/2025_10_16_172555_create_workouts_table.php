0e69b14c2cbf4d03ce4802a9e405cd07d59c9181
Nikola Nikcevic <nikola0511@hotmail.com>
2025-10-16 23:37:17 +0200

    Zaboravio sam da sacuvam promene, ista tema kao prosli commit

--- FlexFlow/database/migrations/2025_10_16_172555_create_workouts_table.php
+++ FlexFlow/database/migrations/2025_10_16_172555_create_workouts_table.php
@@ -14,14 +14,11 @@ return new class extends Migration
         Schema::create('workouts', function (Blueprint $table) {
             $table->id('workoutid');
             $table->unsignedBigInteger('userid');
-            $table->unsignedBigInteger('exerciseid');
             $table->date('date');
-            $table->integer('sets');
-            $table->integer('reps');
+            $table->text('notes')->nullable();
             $table->timestamps();
 
             $table->foreign('userid')->references('userid')->on('users')->onDelete('cascade');
-            $table->foreign('exerciseid')->references('exerciseid')->on('exercise')->onDelete('cascade');
         });
     }
 
